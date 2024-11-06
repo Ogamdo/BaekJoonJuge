55c8e82be4836a2790abd0b400ee8843e311b602
Ogamdo <91306764+Ogamdo@users.noreply.github.com>
2024-11-06 22:59:17 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 48 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+
+string inputStr = Console.ReadLine();
+switch(inputStr){
+    case "SONGDO":
+    Console.WriteLine("HIGHSCHOOL");
+    break;
+
+    case "CODE":
+    Console.WriteLine("MASTER");
+    break;
+
+    case "2023":
+    Console.WriteLine("0611");
+    break;
+    
+    case "ALGORITHM":
+    Console.WriteLine("CONTEST");
+    break;
+
+    default:
+    break;
+}
+Console.WriteLine("");
