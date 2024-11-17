0ee6f22d1506fa3dc9300591378175be2360d563
Ogamdo <91306764+Ogamdo@users.noreply.github.com>
2024-11-17 19:47:36 +0900

    [Bronze V] Title: Rook, Time: 48 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cs
@@ -0,0 +1,19 @@
+  string[] lines = new string[13];
+lines[0]="  ___  ___  ___";
+lines[1]="  | |__| |__| |";
+lines[2]="  |           |";
+lines[3] =@"   \_________/";
+lines[4] =@"    \_______/";
+lines[5] =@"     |     |";
+lines[6] =@"     |     |";
+lines[7] =@"     |     |";
+lines[8] =@"     |     |";
+lines[9]=  "     |_____|";
+lines[10]=@"  __/       \__";
+lines[11]=@" /             \";
+lines[12]=@"/_______________\";
+
+for(int i=0; i<13;i++)
+{
+    Console.WriteLine(lines[i]);
+}
